f9d2daf19402de5653024de3a6a318f9386cfe70
luongvanket <22021182@vnu.edu.vn>
2023-02-21 16:14:48 +0700

    first init

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -20,7 +20,6 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return 0.0;
     return cos(x);
 }
 
@@ -32,7 +31,6 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
     return sin(x);
 }
 
@@ -48,10 +46,8 @@ double mySqrt(double x) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
+    
+    
     return sqrt(x);
-    
-    
-
-    
     return 0;
 }
